8930566f3026372631a14ac02d64bdf6ec21cb56
sean barclay <fudd1011@hotmail.com>
2015-04-14 17:52:21 -0700

    multi-user overlay
    load up the #tracking page view to load up the modal with the
    scrollable list. It was made to be responsive and resize based on the
    height of the screen.

--- public/script/app.js
+++ public/script/app.js
@@ -1,3 +1,50 @@
+Colors = {};
+Colors.names = {
+    // aqua: "#00ffff",
+    azure: "#f0ffff",
+    beige: "#f5f5dc",
+    // black: "#000000",
+    // blue: "#0000ff",
+    brown: "#a52a2a",
+    // cyan: "#00ffff",
+    // darkblue: "#00008b",
+    // darkcyan: "#008b8b",
+    darkgrey: "#a9a9a9",
+    darkgreen: "#006400",
+    darkkhaki: "#bdb76b",
+    darkmagenta: "#8b008b",
+    darkolivegreen: "#556b2f",
+    darkorange: "#ff8c00",
+    darkorchid: "#9932cc",
+    darkred: "#8b0000",
+    darksalmon: "#e9967a",
+    darkviolet: "#9400d3",
+    fuchsia: "#ff00ff",
+    gold: "#ffd700",
+    green: "#008000",
+    indigo: "#4b0082",
+    khaki: "#f0e68c",
+    // lightblue: "#add8e6",
+    // lightcyan: "#e0ffff",
+    lightgreen: "#90ee90",
+    lightgrey: "#d3d3d3",
+    lightpink: "#ffb6c1",
+    lightyellow: "#ffffe0",
+    lime: "#00ff00",
+    magenta: "#ff00ff",
+    maroon: "#800000",
+    navy: "#000080",
+    olive: "#808000",
+    orange: "#ffa500",
+    pink: "#ffc0cb",
+    purple: "#800080",
+    violet: "#800080",
+    red: "#ff0000",
+    // silver: "#c0c0c0",
+    // white: "#ffffff",
+    yellow: "#ffff00"
+};
+
 // create our angular app and inject ngAnimate and ui-router 
 // =============================================================================
 angular.module('locationTrackingApp', ['ngAnimate', 'ngRoute'])
@@ -85,7 +132,7 @@ angular.module('locationTrackingApp', ['ngAnimate', 'ngRoute'])
 .controller('locationSignInController', function($scope, $location, authService) {
 
     $scope.transEnter = function() {
-        $('.sign-in-btn').on('click', function(){
+        $('.sign-in-btn').on('click', function() {
             $('.sign-in-btn').attr('disabled', 'disabled');
             authService.login($scope.login, $scope.password);
             $scope.$on('auth:updated', function() {
@@ -178,6 +225,41 @@ angular.module('locationTrackingApp', ['ngAnimate', 'ngRoute'])
             }).on('stopfollowing', function() {
                 mapTracker.off('dragstart', lc._stopFollowing, lc);
             });
+
+            Colors.random = function() {
+                var result;
+                var count = 0;
+                for (var prop in this.names)
+                    if (Math.random() < 1 / ++count)
+                        result = prop;
+                return result;
+            };
+
+
+            var names = ['Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai, Jani', 'Hege', 'Kai'];
+            var userList = [];
+
+            for (var i = names.length - 1; i >= 0; i--) {
+                userList.push({
+                    name: names[i],
+                    color: Colors.random()
+                });
+            };
+
+            $scope.names = userList;
+
+            $scope.$apply();
+
+            console.log($scope.names);
+
+            $('a.multi-users').on("click", function(event) {
+                // $(this)
+                event.preventDefault();
+                console.log("yo");
+            });
+
+
+
 
         } else {
             alert("Geolocation IS NOT available!");
@@ -454,8 +536,8 @@ angular.module('locationTrackingApp', ['ngAnimate', 'ngRoute'])
 
 /* cloudant db storage for result map */
 .factory('pouchResult', ["remotedb", function(remotedb) {
-        return new PouchDB(remotedb);
-    }])
+    return new PouchDB(remotedb);
+}])
 
 
 /* cloudant authentication service */
